a199ef910f6f1841f23ab2480b9d0da0c0425d69
nay <naylanurulamalia@gmail.com>
2025-03-25 00:27:16 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,38 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\CourtController;
+use App\Http\Controllers\ReservationController;
+
+/*
+|-------------------------------------------------------------------------
+-
+| Web Routes
+|-------------------------------------------------------------------------
+-
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resources([
+        'roles' => RoleController::class,
+        'users' => UserController::class,
+        'reservations' => ReservationController::class,
+        'courts' => CourtController::class,
+    ]);
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
