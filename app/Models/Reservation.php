4a09c863584bb25ecd62e9e2539f51a08c960a33
Mirandaa <mirandacvh27@gmail.com>
2025-03-24 23:51:07 +0700

    membuat model reservasi

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    use HasFactory;
+
+    // Tentukan nama tabel jika tidak sesuai dengan konvensi Laravel
+    protected $table = 'reservations';
+
+    // Tentukan kolom yang dapat diisi (mass assignable)
+    protected $fillable = [
+        'courts_id',
+        'nama',
+        'tanggal',
+        'jam_mulai',
+        'jam_selesai',
+        'total_harga',
+    ];
+
+    // Relasi dengan model Lapangan
+    public function court()
+    {
+        return $this->belongsTo(Court::class, 'courts_id');
+    }
 }
