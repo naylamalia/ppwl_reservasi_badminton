dee0475004ab6b6767b499c6529b5db4a060d748
Tjut Najmi <tjutnajmi@gmail.com>
2025-03-24 23:03:17 +0700

    update laravel sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
+            'auth:sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
     })
 
